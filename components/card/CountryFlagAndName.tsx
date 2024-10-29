c7794c66908261f85c86967758939a4ad1c20be6
Jean Marc Boulos <jmboulos@outlook.com>
2024-10-29 18:23:23 +0200

    SubmitReviews component coding -29/10/2024

--- components/card/CountryFlagAndName.tsx
+++ components/card/CountryFlagAndName.tsx
@@ -1,4 +1,5 @@
 import { findCountryByCode } from "@/utils/countries";
+import Flag from "react-world-flags";
 
 function CountryFlagAndName({ countryCode }: { countryCode: string }) {
   const validCountry = findCountryByCode(countryCode);
@@ -8,7 +9,8 @@ function CountryFlagAndName({ countryCode }: { countryCode: string }) {
       : validCountry!.name;
   return (
     <span className="flex justify-between items-center gap-2 text-sm ">
-      {validCountry?.flag} {countryName}
+      <Flag code={countryCode} style={{ width: 20, height: 20 }} />{" "}
+      {countryName}
     </span>
   );
 }
