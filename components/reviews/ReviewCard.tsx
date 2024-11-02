9fd5966fa8a605f14fced716ed314c6ee99e71cd
Jean Marc Boulos <jmboulos@outlook.com>
2024-11-02 08:50:14 +0200

    ReviewCard Component -02/11/2024

--- components/reviews/ReviewCard.tsx
+++ components/reviews/ReviewCard.tsx
@@ -21,9 +21,19 @@ function ReviewCard({ reviewInfo, children }: ReviewCardProps) {
             alt="profile"
             className="w-12 h-12 rounded-full object-cover"
           />
-          <div className="ml-"
+          <div className="ml-4">
+            <h3 className="text-sm font-bold capitalize mb-1">
+              {reviewInfo.name}
+            </h3>
+            <Rating rating={reviewInfo.rating} />
+          </div>
         </div>
       </CardHeader>
+      <CardContent>
+        <Comment comment={reviewInfo.comment} />
+      </CardContent>
+      {/* delete button later */}
+      <div className="absolute top-3 right-3">{children}</div>
     </Card>
   );
 }
